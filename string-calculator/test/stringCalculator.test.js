4f774aa7ceee53d94e76e2403c55c8a1080f635e
ajith-adimai <ajith.adimai@gmail.com>
2024-10-03 22:30:49 +0530

    Step 1 => Test for an Empty String

--- string-calculator/test/stringCalculator.test.js
+++ string-calculator/test/stringCalculator.test.js
@@ -1,1 +1,9 @@
 const add = require('../src/stringCalculator');
+
+// Test cases
+describe("String Calculator", () => {
+  // Test for an Empty String
+  it("should return 0 for an empty string", () => {
+      expect(add("")).toBe(0);
+  });
+});
