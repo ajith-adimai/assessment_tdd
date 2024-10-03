6a36b9c1ea6e3ee09341c7732543ac2e49f4057a
ajith-adimai <ajith.adimai@gmail.com>
2024-10-03 22:34:22 +0530

    Step 2 => Test for One and Two Numbers

--- string-calculator/src/stringCalculator.js
+++ string-calculator/src/stringCalculator.js
@@ -1,6 +1,8 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  return null;
+  const numberArray = numbers.split(",");
+  const sum = numberArray.reduce((acc, num) => acc + parseInt(num), 0);
+  return sum;
 }
 
 module.exports = add;
--- string-calculator/test/stringCalculator.test.js
+++ string-calculator/test/stringCalculator.test.js
@@ -6,4 +6,12 @@ describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
       expect(add("")).toBe(0);
   });
+  // Test for One and Two Numbers 
+  it("should return the number itself if the input contains one number", () => {
+      expect(add("1")).toBe(1);
+  });
+  
+  it("should return the sum of two numbers separated by a comma", () => {
+      expect(add("1,2")).toBe(3);
+  });
 });
